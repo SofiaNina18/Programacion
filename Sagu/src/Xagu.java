3e11e345860528249165ea9d3bebd466e81748f2
SofiaNina18 <Snina.work@gmail.com>
2025-12-12 11:47:25 +0100

    XAGU TERMINADO

--- Sagu/src/Xagu.java
+++ Sagu/src/Xagu.java
@@ -1,6 +1,15 @@
 import java.awt.EventQueue;
 import java.awt.FileDialog;
 import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,28 +17,21 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.BorderLayout;
-import javax.swing.JButton;
-import javax.swing.ImageIcon;
-
 public class Xagu extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	public static final int FILAS = 10;
-	public static final int COLS = 10;
+	public static final int FILAS=10;
+	public static final int COLS=10;
 	private JPanel contentPane;
 	private JButton btnSalir;
+	private JButton btnBuscarSsalida;
 	private JButton btnCargar;
-	private JButton btnBuscarSalida;
-
-	private JLabel[][] casillas;
+	
+	private JLabel [][]casillas;
 	private JPanel panelLaberinto;
 	private int filaXagu, colXagu;
 	private boolean salidaEncontrada;
+	
 
 	/**
 	 * Launch the application.
@@ -51,58 +53,53 @@ public class Xagu extends JFrame {
 	 * Create the frame.
 	 */
 	public Xagu() {
+		setTitle("XaguAlmi");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 633, 458);
+		setBounds(100, 100, 552, 408);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
-
+		
 		JPanel panelBotones = new JPanel();
 		contentPane.add(panelBotones, BorderLayout.NORTH);
-
+		
 		btnCargar = new JButton("Cargar Mapa");
 		panelBotones.add(btnCargar);
-
-		btnBuscarSalida = new JButton("Buscar Salida");
-		panelBotones.add(btnBuscarSalida);
-
+		
+		btnBuscarSsalida = new JButton("Buscar Salida");
+		panelBotones.add(btnBuscarSsalida);
+		
 		btnSalir = new JButton("Salir");
 		panelBotones.add(btnSalir);
-
+		
 		panelLaberinto = new JPanel();
 		contentPane.add(panelLaberinto, BorderLayout.CENTER);
 		panelLaberinto.setLayout(new GridLayout(FILAS, COLS, 0, 0));
 
 		crearLabels();
 		registrarEventos();
-	}// Fin del constructor
-
+		
+	}//FIN DEL CONSTRUCTOR
+	
 	public void registrarEventos() {
 		btnCargar.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				cargarMapa();
-			}
-		});
-		btnBuscarSalida.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
+				cargarMapa();
+			}
+		});
+		
+		btnBuscarSsalida.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
 				buscarSalida(casillas, filaXagu, colXagu);
 			}
 		});
-		btnSalir.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				System.exit(0);
-			}
-		});
+		
 	}//FIN DE REGISTRAR EVENTOS
-	
 	
 	public void buscarSalida(JLabel [][] casillas, int filaXagu, int colXagu) {
 		if (filaXagu==0 || filaXagu==FILAS-1 || colXagu==0 || colXagu==COLS-1) {
@@ -131,66 +128,57 @@ public class Xagu extends JFrame {
 		if (!salidaEncontrada && casillas[filaXagu][colXagu-1].getText().equals("")) {
 			buscarSalida(casillas, filaXagu, colXagu-1);
 			}
-		if (!salidaEncontrada) {
+		if (salidaEncontrada) {
 			casillas[filaXagu][colXagu].setText("$");
 		}
 		
-		
-		
 	}
-		
 
 	protected void cargarMapa() {
-		// TODO Auto-generated method stub
-
 		Scanner scFich;
 		FileDialog dlgMapa;
 		String linea;
 		String fich;
 		salidaEncontrada=false;
-
-		dlgMapa = new FileDialog(this, "Cargar Mapa", FileDialog.LOAD);
-		// Antes de mostrar el dialogo, podemos configurar algunas cosas
-		// dlgMapa.setDirectory(System.getProperty("user.home")+"\\Desktop");
-		// //Directorio por defecto
-		dlgMapa.setDirectory(".\\img"); // Directorio por defecto,
-										// se pone punto porque indica que dentro
-										// de la carpeta actual entre al img
-		// añadir filtros
-		dlgMapa.setVisible(true);
-		fich = dlgMapa.getFile();
 		
-		if (fich!=null) {
+		dlgMapa=new FileDialog(this, "Cargar Mapa", FileDialog.LOAD);
+		//dlgMapa.setDirectory(System.getProperty("user.home")+"\\Desktop");
+		dlgMapa.setDirectory(".\\img");
+		dlgMapa.setVisible(true);
+		fich=dlgMapa.getFile();
+		
+		if(fich!=null) {
 			try {
-				scFich = new Scanner(new File(dlgMapa.getDirectory() + fich));
-				//scFich=new Scanner(new File(".\\img\\"+fich));
-				int fila = 0;
-				while (scFich.hasNext()) {
-					//LEER UNA LINEA
-					linea = scFich.nextLine();
-					//PROCESARLA Linea
-					for (int col = 0; col < COLS; col++) {
-						//Si es "0" -> quitar * (vaciar casilla)
-						//si es 1 -> Cargar imagen de muro.jpg
-						//si es 2 -> Cargar imagen de Xagu.jpg
-						if (linea.charAt(col) == '0') {
+				scFich=new Scanner(new File(dlgMapa.getDirectory()+fich));
+				//scFich=new Scanner(getClass().getResource(fich));
+				int fila=0;
+				while(scFich.hasNext()) {
+					//LEER LINEA
+					linea=scFich.nextLine();
+					//PROCESAR LINEA
+					for(int col=0;col<COLS;col++) {
+						//SI ES "0" -> Quitar * (vaciar casilla)
+						//SI ES UN 1 -> Cargar Imagen MURO.JPG
+						//SI ES UN 2 -> Cargar Imagen XAGU.JPG
+						if(linea.charAt(col)=='0') {
 							casillas[fila][col].setText("");
-							casillas[fila][col].setIcon(null); //hace que desaparezca la imagen
-							//que el mapa solo se active en uno mapa
-							
-						} if (linea.charAt(col) == '1') 
-							//Cargar fichero de imagen
-						{
+							casillas[fila][col].setIcon(null);
+						}
+						if(linea.charAt(col)=='1') {
+							//cargar Fichero imagen en esa casilla
 							//casillas[fila][col].setIcon(new ImageIcon("./img/muro.jpg"));
+							//cargar el fichero como recurso
 							casillas[fila][col].setIcon(new ImageIcon(getClass().getResource("muro.jpg")));
 							casillas[fila][col].setText("*");
-						} if (linea.charAt(col) == '2') {
+						}
+						if(linea.charAt(col)=='2') {
 							casillas[fila][col].setIcon(new ImageIcon(getClass().getResource("xagu.jpg")));
 							casillas[fila][col].setText("*");
 							filaXagu=fila;
 							colXagu=col;
 						}
-					} fila++;
+					}
+					fila++;
 				}
 				scFich.close();
 			} catch (FileNotFoundException e) {
@@ -198,30 +186,33 @@ public class Xagu extends JFrame {
 				e.printStackTrace();
 			}
 		}
-
+		
 	}
-
-//Vamos a crear objetos en ejecusion
 
 	public void crearLabels() {
 		JLabel lblAux;
-		// Vamos a hacer un array bidemensional de JLabels
-
-		casillas = new JLabel[FILAS][COLS];
-		// CREAR LOS 100 JLABELS con sus caracteristicas
-		// Texto,tamaño y posicion, color, etc....
-		// Añadirlo al panel
-		// Añadirlo al array
-		for (int fila = 0; fila < FILAS; fila++) {
-			for (int col = 0; col < COLS; col++) {
-				lblAux = new JLabel("*");
+		//DIMENSIONAR EL ARRAY DE CASILLAS (instanciar)
+		casillas=new JLabel[FILAS][COLS];
+		//crear los 100 JLabels con sus caracterírsticas:
+			//Texto, tamaño, posición, color, etc...
+			//Añadirlo al panel
+			//Añadirlo al array
+		for(int fila=0;fila<FILAS;fila++) {
+			for(int col=0;col<COLS;col++) {
+				lblAux=new JLabel("*");
 				lblAux.setHorizontalAlignment(JLabel.CENTER);
 				lblAux.setFont(new Font("Arial", Font.BOLD, 20));
 				panelLaberinto.add(lblAux);
-				casillas[fila][col] = lblAux;
+				casillas[fila][col]=lblAux;
 			}
-		} 
-
+		}
 	}
+	
+	
 
 }
+
+
+
+
+
